05421ffae9c5438477df59d9d59cfb3c4e1caf35
Katie Anderson <katherine.lizf@gmail.com>
2022-03-23 16:59:21 -0500

    Initial commit

---
+++ calc.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+console.log('mathSymbol', mathSymbol);
+console.log('num1', num1);
+console.log('num2', num2);
+
+if (mathSymbol === "+") {
+	console.log(num1 + num2);
+} else if (mathSymbol === "-") {
+	console.log(num1 - num2) ;
+} else if (mathSymbol === "*") {
+	console.log(num1 * num2);
+} else if (mathSymbol === "/") {
+	console.log(num1 / num2);
+} else if (mathSymbol === "sqrt") {
+	console.log(Math.sqrt(num1));
+} else if (mathSymbol === "sq") {
+	console.log(num1 * num1);
+} else if (mathSymbol === "cube") {
+	console.log(num1 * num1 * num1);
+} else if (mathSymbol === "power") {
+	console.log(Math.pow(num1, num2));
+} else if (mathSymbol === "remainder") {
+	console.log(num1 % num2);
+}
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
